53c5e5d536ff8fd8df6a8679fbf298b7af7eb026
Gurdeep Singh <gurdeepnz19@gmail.com>
2025-07-10 10:04:04 +1200

    new changes

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import ordersCreateHandler from './ordersCreate.js';
+
+const app = express();
+
+// Use raw body parser only on webhook route to verify HMAC correctly
+app.post(
+    '/webhooks/orders-create',
+    express.raw({ type: 'application/json' }),
+    ordersCreateHandler
+);
+
+
+const PORT = process.env.PORT || 3000;
+console.log("✅ Environment variables loaded:", {
+    SHOPIFY_SHARED_SECRET: process.env.SHOPIFY_SHARED_SECRET,
+    SHOPIFY_ACCESS_TOKEN: process.env.SHOPIFY_ACCESS_TOKEN,
+    SHOPIFY_SHOP: process.env.SHOPIFY_SHOP
+});
+app.listen(PORT, () => console.log(`🚀 Webhook server running on port ${PORT}`));
