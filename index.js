9b7c9ea4573e6d8de395b0ba29b4214bd7bbb15d
Gurdeep Singh <gurdeepnz19@gmail.com>
2025-07-11 10:02:39 +1200

    new changes for render

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿require('dotenv').config();
+const express = require('express');
+const ordersUpdate = require('./api/orders-update');
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.post('/webhooks/orders-update', express.raw({ type: 'application/json' }), ordersUpdate);
+
+app.listen(port, () => {
+    console.log(`🚀 Server running on port ${port}`);
+});
